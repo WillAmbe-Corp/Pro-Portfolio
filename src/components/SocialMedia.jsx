db1704e8a4610886e8fc37e4da0c81564e49a6ee
unknown <nebaprincewill237@gmail.com>
2025-06-09 04:08:12 +0100

    adding social media icons

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -8,7 +8,7 @@ const socialMedia = [
 
 export default function SocialMedia() {
     return (
-        <ul className="flex gap-12 text-sm lg:absolute lg:bottom-10 lg:-right-18 lg:rotate-270">
+        <ul className="flex gap-12 text-sm lg:absolute lg:bottom-10 lg:-right-15 lg:rotate-270">
             {socialMedia.map(({ Medium, Link }) => (
                 <li key={Medium}>
                     <a href={Link} target="_blank" rel="noopener noreferrer" className="hover:text-gray-600">
